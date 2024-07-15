4485d1f1373f3620e56ebd98b1abb3d9c55e22d3
Zakwan_Shaikh / Fullstack Developer <113516469+demo12lti@users.noreply.github.com>
2024-07-15 06:13:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const priceRoutes = require('./routes/priceRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
